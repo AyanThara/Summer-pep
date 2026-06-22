82ab41015929633a2f4f48c7d0267fb1278fea2f
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-06-22 23:43:45 +0530

    Create 06_Loops.cpp

---
+++ cpp/06_Loops.cpp
@@ -0,0 +1,42 @@
+//loops
+//for loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+    return 0;
+}
+
+//while loop
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    while(i <= 5) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+
+    do {
+        cout << i << " ";
+        i++;
+    } while(i <= 5);
+
+    return 0;
+}*/
