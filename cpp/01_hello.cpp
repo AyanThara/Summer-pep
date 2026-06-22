0420ffd7668f571711b8cde5a58261eff65c50a8
Ayan Thara <ayanthara786@gmail.com>
2026-06-22 10:56:21 +0530

    Added first C++ hello world program

---
+++ cpp/01_hello.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//data type
+//int -> 4 bytes
+//float -> 4 bytes
+//double -> 8 bytes     
+//char -> 1 byte
+// long long int -> 8 bytes
+   int age;
+   char name='A';
+   bool isAdult=true;
+   age=20;
+   cout<<age;
+    return 0;
+}
