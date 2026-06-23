71c3d78fd4dfde06ed3ef29f996e4dd4a997dac1
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-06-23 11:25:55 +0530

    Create 02_Vectors.cpp

---
+++ Dsa/Array/02_Vectors.cpp
@@ -0,0 +1,22 @@
+//vector
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);// to add an element at the last 
+    v.pop_back();//to remove an element 
+    v.front();// to check the first elemeent 
+    v.back();//to get the element at the last 
+    v.size();// to calculate the size
+}
+
+some more vector functions 
+v.empty();     // true if empty
+v.clear();     // remove all elements
+v.at(0);       // check the element at the 0 th index 
+
+*/
+
