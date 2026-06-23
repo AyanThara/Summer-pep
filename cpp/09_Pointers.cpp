e4ea6d0edd7a52da8dce4e83ae9b48219d18460b
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-06-23 11:23:18 +0530

    Create 09_Pointers.cpp

---
+++ cpp/09_Pointers.cpp
@@ -0,0 +1,12 @@
+//pointer
+#include <iostream>
+using namespace std;            
+int main() {                    
+    int a=10;    
+    int *ptr=&a;  
+    cout<<"value of a is: "<<a<<endl;
+    cout<<"address of a is: "<<&a<<endl;
+    cout<<"value of ptr is: "<<ptr<<endl;       
+    cout<<"value at address ptr is: "<<*ptr<<endl;    //dereferencing  a pointer
+}
+*/
