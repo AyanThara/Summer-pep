bbc198efb3fe60ecd386a19b88539beed6ba76a3
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-06-24 09:43:25 +0530

    Create Second_Largest.cpp

---
+++ Gfg/Second_Largest.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getSecondLargest(vector<int> &arr) {
+        int largest = INT_MIN;
+        int second = INT_MIN;
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] > largest) {
+                second = largest;
+                largest = arr[i];
+            }
+            else if(arr[i] > second && arr[i] < largest) {
+                second = arr[i];
+            }
+        }
+        if(second == INT_MIN)
+            return -1;
+        return second;
+    }
+};
