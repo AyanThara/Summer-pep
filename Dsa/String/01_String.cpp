2a1dbc9c2138a0ffb22319b2b9e209faa634d4ca
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-07-01 11:22:41 +0530

    Create 01_String.cpp

---
+++ Dsa/String/01_String.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    string str = "Hello World";
+    string str1 = "hello world";
+    if(str == str1) {
+        cout << "Strings are equal" << endl;
+    } else {
+        cout << "Strings are not equal" << endl;
+    }
+}
